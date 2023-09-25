7f255aa6f52832c9bfabb92b8b5d8ff6c61d7ee0
PHChenGit <chenpohsun1@gmail.com>
2023-09-25 22:33:07 +0800

    feat: Add assigment_1 material

---
+++ 112/Homeworks/HW_1/LabelCC.m
@@ -0,0 +1,17 @@
+function result = LabelCC(img, num)
+% component labeling with num-connected (num =4 or 8)
+
+    if num == 4
+        % ------ implement your 4-connected components labeling algorithm here ------
+        % result = your answer 
+
+
+    elseif num ==8
+        % ------ implement your 8-connected components labeling algorithm here ------
+        % result = your answer 
+
+
+    else
+        error('MyFunc:InputMustbe4or8','The second argument should be 4 or 8.')
+    end
+end
