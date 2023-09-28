1e28d02c8efea12dc2543ebd0dc46470234f8665
PHChenGit <chenpohsun1@gmail.com>
2023-09-28 12:34:02 +0800

    feat: Label 8-connected components

--- 112/Homeworks/HW_1/LabelCC.m
+++ 112/Homeworks/HW_1/LabelCC.m
@@ -26,6 +26,18 @@ function result = LabelCC(img, num)
         % ------ implement your 8-connected components labeling algorithm here ------
         % result = your answer 
 
+        for r = 1:rows
+            for c = 1:cols
+                if ~visited(r, c) && img(r, c) > 0
+                    labelCounter = labelCounter + 1;
+                    [newVisited, newLabels] = DFS_8Connected(r, c, img, labelCounter, visited, labels);
+                    visited = newVisited;
+                    labels = newLabels;
+                end
+            end
+        end
+
+        result = labels;
 
     else
         error('MyFunc:InputMustbe4or8','The second argument should be 4 or 8.')
@@ -47,6 +59,32 @@ function [newVisited, newLabels] = DFS_4Connected(x, y, img, labelCnt, visited, 
         [visited, labels] = DFS_4Connected(x, y-1, img, labelCnt, visited, labels);
         [visited, labels] = DFS_4Connected(x-1, y, img, labelCnt, visited, labels);
         [visited, labels] = DFS_4Connected(x+1, y, img, labelCnt, visited, labels);
+    end
+
+    newVisited = visited;
+    newLabels = labels;
+end
+
+function [newVisited, newLabels] = DFS_8Connected(x, y, img, labelCnt, visited, labels)
+    [rows, cols] = size(img);
+
+    if x >= 1 && x <= rows && ...
+        y >= 1 && y <= cols && ...
+        ~visited(x, y) && ...
+        img(x, y) ~= 0
+
+        visited(x, y) = true;
+        labels(x, y) = labelCnt;
+
+        [visited, labels] = DFS_8Connected(x, y+1, img, labelCnt, visited, labels);
+        [visited, labels] = DFS_8Connected(x, y-1, img, labelCnt, visited, labels);
+        [visited, labels] = DFS_8Connected(x-1, y, img, labelCnt, visited, labels);
+        [visited, labels] = DFS_8Connected(x+1, y, img, labelCnt, visited, labels);
+
+        [visited, labels] = DFS_8Connected(x-1, y+1, img, labelCnt, visited, labels);
+        [visited, labels] = DFS_8Connected(x-1, y-1, img, labelCnt, visited, labels);
+        [visited, labels] = DFS_8Connected(x+1, y+1, img, labelCnt, visited, labels);
+        [visited, labels] = DFS_8Connected(x+1, y-1, img, labelCnt, visited, labels);
     end
 
     newVisited = visited;
